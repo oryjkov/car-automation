31c784e52ee508b365281a12de6cebd179b62c30
olegr <oryjkov@gmail.com>
2022-06-21 17:47:36 +0200

    8255

--- victron-esphome/flash.sh
+++ victron-esphome/flash.sh
@@ -1,1 +1,3 @@
-docker run --rm -v "${PWD}":/config --network host -it esphome/esphome run victron.yaml
+#docker run --rm -v "${PWD}":/config --network host -it esphome/esphome run victron.yaml
+# To flash via usb serial.
+docker run --rm -v "${PWD}":/config --device=/dev/ttyUSB0 --network host -it esphome/esphome run victron.yaml
