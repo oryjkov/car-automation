d4e8dc836a168c04d7b8808a9cce9e9d27f976d8
olegr <oryjkov@gmail.com>
2022-06-22 22:12:39 +0200

    daly configs and debug logs

--- victron-esphome/flash.sh
+++ victron-esphome/flash.sh
@@ -1,3 +1,6 @@
 #docker run --rm -v "${PWD}":/config --network host -it esphome/esphome run victron.yaml
 # To flash via usb serial.
-docker run --rm -v "${PWD}":/config --device=/dev/ttyUSB0 --network host -it esphome/esphome run victron.yaml
+#docker run --rm -v "${PWD}":/config --device=/dev/ttyUSB0 --network host -it esphome/esphome run victron.yaml
+#docker run --rm -v "${PWD}":/config --device=/dev/ttyUSB0 --network host -it esphome/esphome run daly.yaml
+#docker run --rm -v "${PWD}":/config --device=/dev/ttyUSB0 --network host -it esphome/esphome run "$1"
+docker run --rm -v "${PWD}":/config --network host -it esphome/esphome run "$1"
